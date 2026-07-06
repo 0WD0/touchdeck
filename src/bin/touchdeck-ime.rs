b51e8b6d1861fc8ed555c729044465ae2813e786
0WD0 <wd.1105848296@gmail.com>
2026-07-06 10:22:28 +0800

    Extract touchdeck-ime XIM frontend

--- src/bin/touchdeck-ime.rs
+++ src/bin/touchdeck-ime.rs
@@ -3,7 +3,6 @@ use std::os::fd::{AsFd, AsRawFd, RawFd};
 use std::os::raw::c_int;
 use std::path::PathBuf;
 use std::sync::mpsc::{self, Sender};
-use std::thread;
 use std::time::Duration;
 
 use anyhow::{anyhow, Context, Result};
@@ -24,10 +23,6 @@ use wayland_protocols_misc::zwp_virtual_keyboard_v1::client::{
     zwp_virtual_keyboard_manager_v1::{self, ZwpVirtualKeyboardManagerV1},
     zwp_virtual_keyboard_v1::{self, ZwpVirtualKeyboardV1},
 };
-use x11rb::connection::Connection as X11Connection;
-use x11rb::protocol::xproto::{KeyPressEvent, KEY_PRESS_EVENT};
-use xim::x11rb::HasConnection;
-use xim::{InputStyle, Server, ServerHandler, UserInputContext, XimConnections};
 
 #[path = "touchdeck-ime/fcitx_dbus.rs"]
 mod fcitx_dbus;
@@ -41,6 +36,8 @@ mod popup;
 mod rime_engine;
 #[path = "touchdeck-ime/touchdeck_socket.rs"]
 mod touchdeck_socket;
+#[path = "touchdeck-ime/xim_frontend.rs"]
+mod xim_frontend;
 
 use fcitx_dbus::*;
 use ime_config::*;
@@ -48,6 +45,7 @@ use ime_key::*;
 use popup::*;
 use rime_engine::*;
 use touchdeck_socket::*;
+use xim_frontend::*;
 
 #[derive(Default)]
 struct ImeApp {
@@ -78,26 +76,6 @@ struct ImeApp {
     physical_modifiers: u32,
     virtual_keyboard_has_keymap: bool,
     running: bool,
-}
-
-enum XimRequest {
-    Key {
-        time: u32,
-        hardware_keycode: u8,
-        state_mask: u16,
-        state: KeyState,
-        response: Sender<XimKeyResponse>,
-    },
-    Reset {
-        response: Sender<String>,
-    },
-}
-
-#[derive(Debug, Default)]
-struct XimKeyResponse {
-    consumed: bool,
-    preedit: String,
-    commit: Option<String>,
 }
 
 fn main() -> Result<()> {
@@ -1381,206 +1359,6 @@ impl Dispatch<ZwpVirtualKeyboardV1, ()> for ImeApp {
         _conn: &Connection,
         _qh: &QueueHandle<Self>,
     ) {
-    }
-}
-
-const XIM_EVENT_MASK: u32 = 3;
-
-struct TouchDeckXimHandler {
-    tx: Sender<XimRequest>,
-}
-
-impl TouchDeckXimHandler {
-    fn new(tx: Sender<XimRequest>) -> Self {
-        Self { tx }
-    }
-
-    fn request_reset(&self) -> String {
-        let (response_tx, response_rx) = mpsc::channel();
-        if self
-            .tx
-            .send(XimRequest::Reset {
-                response: response_tx,
-            })
-            .is_err()
-        {
-            return String::new();
-        }
-        response_rx
-            .recv_timeout(Duration::from_millis(500))
-            .unwrap_or_default()
-    }
-}
-
-impl<C: xim::x11rb::HasConnection> ServerHandler<xim::x11rb::X11rbServer<C>>
-    for TouchDeckXimHandler
-{
-    type InputContextData = ();
-    type InputStyleArray = [InputStyle; 6];
-
-    fn new_ic_data(
-        &mut self,
-        _server: &mut xim::x11rb::X11rbServer<C>,
-        _input_style: InputStyle,
-    ) -> std::result::Result<Self::InputContextData, xim::ServerError> {
-        Ok(())
-    }
-
-    fn input_styles(&self) -> Self::InputStyleArray {
-        [
-            InputStyle::PREEDIT_NOTHING | InputStyle::STATUS_NOTHING,
-            InputStyle::PREEDIT_POSITION | InputStyle::STATUS_NONE,
-            InputStyle::PREEDIT_POSITION | InputStyle::STATUS_NOTHING,
-            InputStyle::PREEDIT_POSITION | InputStyle::STATUS_CALLBACKS,
-            InputStyle::PREEDIT_CALLBACKS | InputStyle::STATUS_NOTHING,
-            InputStyle::PREEDIT_CALLBACKS | InputStyle::STATUS_CALLBACKS,
-        ]
-    }
-
-    fn filter_events(&self) -> u32 {
-        XIM_EVENT_MASK
-    }
-
-    fn handle_connect(
-        &mut self,
-        _server: &mut xim::x11rb::X11rbServer<C>,
-    ) -> std::result::Result<(), xim::ServerError> {
-        eprintln!("touchdeck-ime: xim client connected");
-        Ok(())
-    }
-
-    fn handle_create_ic(
-        &mut self,
-        server: &mut xim::x11rb::X11rbServer<C>,
-        user_ic: &mut UserInputContext<Self::InputContextData>,
-    ) -> std::result::Result<(), xim::ServerError> {
-        eprintln!("touchdeck-ime: xim create input context");
-        server.set_event_mask(&user_ic.ic, XIM_EVENT_MASK, 0)
-    }
-
-    fn handle_destroy_ic(
-        &mut self,
-        _server: &mut xim::x11rb::X11rbServer<C>,
-        _user_ic: UserInputContext<Self::InputContextData>,
-    ) -> std::result::Result<(), xim::ServerError> {
-        eprintln!("touchdeck-ime: xim destroy input context");
-        Ok(())
-    }
-
-    fn handle_reset_ic(
-        &mut self,
-        _server: &mut xim::x11rb::X11rbServer<C>,
-        _user_ic: &mut UserInputContext<Self::InputContextData>,
-    ) -> std::result::Result<String, xim::ServerError> {
-        eprintln!("touchdeck-ime: xim reset input context");
-        Ok(self.request_reset())
-    }
-
-    fn handle_set_focus(
-        &mut self,
-        _server: &mut xim::x11rb::X11rbServer<C>,
-        _user_ic: &mut UserInputContext<Self::InputContextData>,
-    ) -> std::result::Result<(), xim::ServerError> {
-        eprintln!("touchdeck-ime: xim focus in");
-        Ok(())
-    }
-
-    fn handle_unset_focus(
-        &mut self,
-        _server: &mut xim::x11rb::X11rbServer<C>,
-        _user_ic: &mut UserInputContext<Self::InputContextData>,
-    ) -> std::result::Result<(), xim::ServerError> {
-        eprintln!("touchdeck-ime: xim focus out");
-        self.request_reset();
-        Ok(())
-    }
-
-    fn handle_set_ic_values(
-        &mut self,
-        server: &mut xim::x11rb::X11rbServer<C>,
-        user_ic: &mut UserInputContext<Self::InputContextData>,
-    ) -> std::result::Result<(), xim::ServerError> {
-        eprintln!("touchdeck-ime: xim set input context values");
-        server.preedit_draw(&mut user_ic.ic, "")
-    }
-
-    fn handle_forward_event(
-        &mut self,
-        server: &mut xim::x11rb::X11rbServer<C>,
-        user_ic: &mut UserInputContext<Self::InputContextData>,
-        xev: &KeyPressEvent,
-    ) -> std::result::Result<bool, xim::ServerError> {
-        let state = if xev.response_type == KEY_PRESS_EVENT {
-            KeyState::Pressed
-        } else {
-            KeyState::Released
-        };
-        eprintln!(
-            "touchdeck-ime: xim forward key detail={} state={state:?} mask={} time={}",
-            xev.detail,
-            u16::from(xev.state),
-            xev.time
-        );
-        let (response_tx, response_rx) = mpsc::channel();
-        if self
-            .tx
-            .send(XimRequest::Key {
-                time: xev.time,
-                hardware_keycode: xev.detail,
-                state_mask: u16::from(xev.state),
-                state,
-                response: response_tx,
-            })
-            .is_err()
-        {
-            return Ok(false);
-        }
-
-        let response = match response_rx.recv_timeout(Duration::from_millis(500)) {
-            Ok(response) => response,
-            Err(_) => return Ok(false),
-        };
-
-        server.preedit_draw(&mut user_ic.ic, &response.preedit)?;
-        if let Some(commit) = response.commit {
-            eprintln!("touchdeck-ime: xim commit {commit:?}");
-            server.commit(&user_ic.ic, &commit)?;
-        }
-        eprintln!(
-            "touchdeck-ime: xim key consumed={} preedit={:?}",
-            response.consumed, response.preedit
-        );
-        Ok(response.consumed)
-    }
-}
-
-fn spawn_xim_server(tx: Sender<XimRequest>) {
-    thread::spawn(move || {
-        if let Err(err) = run_xim_server(tx) {
-            eprintln!("touchdeck-ime: xim server stopped: {err:?}");
-        }
-    });
-}
-
-fn run_xim_server(tx: Sender<XimRequest>) -> Result<()> {
-    let (conn, screen_num) = x11rb::rust_connection::RustConnection::connect(None)
-        .context("connect to X display for XIM")?;
-    let mut server = xim::x11rb::X11rbServer::init(conn, screen_num, "touchdeck", xim::ALL_LOCALES)
-        .context("initialize XIM server")?;
-    let mut connections = XimConnections::new();
-    let mut handler = TouchDeckXimHandler::new(tx);
-
-    eprintln!("touchdeck-ime: xim server initialized");
-    loop {
-        let event = server.conn().wait_for_event().context("wait for X event")?;
-        match server.filter_event(&event, &mut connections, &mut handler) {
-            Ok(_) => {
-                if let Err(err) = server.conn().flush() {
-                    eprintln!("touchdeck-ime: xim flush error: {err}");
-                }
-            }
-            Err(err) => eprintln!("touchdeck-ime: xim event error: {err}"),
-        }
     }
 }
 
---
+++ src/bin/touchdeck-ime/xim_frontend.rs
@@ -0,0 +1,231 @@
+use std::sync::mpsc::{self, Sender};
+use std::thread;
+use std::time::Duration;
+
+use anyhow::{Context, Result};
+use x11rb::connection::Connection as X11Connection;
+use x11rb::protocol::xproto::{KeyPressEvent, KEY_PRESS_EVENT};
+use xim::x11rb::HasConnection;
+use xim::{InputStyle, Server, ServerHandler, UserInputContext, XimConnections};
+
+use super::ime_key::KeyState;
+
+const XIM_EVENT_MASK: u32 = 3;
+
+pub(super) enum XimRequest {
+    Key {
+        time: u32,
+        hardware_keycode: u8,
+        state_mask: u16,
+        state: KeyState,
+        response: Sender<XimKeyResponse>,
+    },
+    Reset {
+        response: Sender<String>,
+    },
+}
+
+#[derive(Debug, Default)]
+pub(super) struct XimKeyResponse {
+    pub(super) consumed: bool,
+    pub(super) preedit: String,
+    pub(super) commit: Option<String>,
+}
+
+struct TouchDeckXimHandler {
+    tx: Sender<XimRequest>,
+}
+
+impl TouchDeckXimHandler {
+    fn new(tx: Sender<XimRequest>) -> Self {
+        Self { tx }
+    }
+
+    fn request_reset(&self) -> String {
+        let (response_tx, response_rx) = mpsc::channel();
+        if self
+            .tx
+            .send(XimRequest::Reset {
+                response: response_tx,
+            })
+            .is_err()
+        {
+            return String::new();
+        }
+        response_rx
+            .recv_timeout(Duration::from_millis(500))
+            .unwrap_or_default()
+    }
+}
+
+impl<C: xim::x11rb::HasConnection> ServerHandler<xim::x11rb::X11rbServer<C>>
+    for TouchDeckXimHandler
+{
+    type InputContextData = ();
+    type InputStyleArray = [InputStyle; 6];
+
+    fn new_ic_data(
+        &mut self,
+        _server: &mut xim::x11rb::X11rbServer<C>,
+        _input_style: InputStyle,
+    ) -> std::result::Result<Self::InputContextData, xim::ServerError> {
+        Ok(())
+    }
+
+    fn input_styles(&self) -> Self::InputStyleArray {
+        [
+            InputStyle::PREEDIT_NOTHING | InputStyle::STATUS_NOTHING,
+            InputStyle::PREEDIT_POSITION | InputStyle::STATUS_NONE,
+            InputStyle::PREEDIT_POSITION | InputStyle::STATUS_NOTHING,
+            InputStyle::PREEDIT_POSITION | InputStyle::STATUS_CALLBACKS,
+            InputStyle::PREEDIT_CALLBACKS | InputStyle::STATUS_NOTHING,
+            InputStyle::PREEDIT_CALLBACKS | InputStyle::STATUS_CALLBACKS,
+        ]
+    }
+
+    fn filter_events(&self) -> u32 {
+        XIM_EVENT_MASK
+    }
+
+    fn handle_connect(
+        &mut self,
+        _server: &mut xim::x11rb::X11rbServer<C>,
+    ) -> std::result::Result<(), xim::ServerError> {
+        eprintln!("touchdeck-ime: xim client connected");
+        Ok(())
+    }
+
+    fn handle_create_ic(
+        &mut self,
+        server: &mut xim::x11rb::X11rbServer<C>,
+        user_ic: &mut UserInputContext<Self::InputContextData>,
+    ) -> std::result::Result<(), xim::ServerError> {
+        eprintln!("touchdeck-ime: xim create input context");
+        server.set_event_mask(&user_ic.ic, XIM_EVENT_MASK, 0)
+    }
+
+    fn handle_destroy_ic(
+        &mut self,
+        _server: &mut xim::x11rb::X11rbServer<C>,
+        _user_ic: UserInputContext<Self::InputContextData>,
+    ) -> std::result::Result<(), xim::ServerError> {
+        eprintln!("touchdeck-ime: xim destroy input context");
+        Ok(())
+    }
+
+    fn handle_reset_ic(
+        &mut self,
+        _server: &mut xim::x11rb::X11rbServer<C>,
+        _user_ic: &mut UserInputContext<Self::InputContextData>,
+    ) -> std::result::Result<String, xim::ServerError> {
+        eprintln!("touchdeck-ime: xim reset input context");
+        Ok(self.request_reset())
+    }
+
+    fn handle_set_focus(
+        &mut self,
+        _server: &mut xim::x11rb::X11rbServer<C>,
+        _user_ic: &mut UserInputContext<Self::InputContextData>,
+    ) -> std::result::Result<(), xim::ServerError> {
+        eprintln!("touchdeck-ime: xim focus in");
+        Ok(())
+    }
+
+    fn handle_unset_focus(
+        &mut self,
+        _server: &mut xim::x11rb::X11rbServer<C>,
+        _user_ic: &mut UserInputContext<Self::InputContextData>,
+    ) -> std::result::Result<(), xim::ServerError> {
+        eprintln!("touchdeck-ime: xim focus out");
+        self.request_reset();
+        Ok(())
+    }
+
+    fn handle_set_ic_values(
+        &mut self,
+        server: &mut xim::x11rb::X11rbServer<C>,
+        user_ic: &mut UserInputContext<Self::InputContextData>,
+    ) -> std::result::Result<(), xim::ServerError> {
+        eprintln!("touchdeck-ime: xim set input context values");
+        server.preedit_draw(&mut user_ic.ic, "")
+    }
+
+    fn handle_forward_event(
+        &mut self,
+        server: &mut xim::x11rb::X11rbServer<C>,
+        user_ic: &mut UserInputContext<Self::InputContextData>,
+        xev: &KeyPressEvent,
+    ) -> std::result::Result<bool, xim::ServerError> {
+        let state = if xev.response_type == KEY_PRESS_EVENT {
+            KeyState::Pressed
+        } else {
+            KeyState::Released
+        };
+        eprintln!(
+            "touchdeck-ime: xim forward key detail={} state={state:?} mask={} time={}",
+            xev.detail,
+            u16::from(xev.state),
+            xev.time
+        );
+        let (response_tx, response_rx) = mpsc::channel();
+        if self
+            .tx
+            .send(XimRequest::Key {
+                time: xev.time,
+                hardware_keycode: xev.detail,
+                state_mask: u16::from(xev.state),
+                state,
+                response: response_tx,
+            })
+            .is_err()
+        {
+            return Ok(false);
+        }
+
+        let response = match response_rx.recv_timeout(Duration::from_millis(500)) {
+            Ok(response) => response,
+            Err(_) => return Ok(false),
+        };
+
+        server.preedit_draw(&mut user_ic.ic, &response.preedit)?;
+        if let Some(commit) = response.commit {
+            eprintln!("touchdeck-ime: xim commit {commit:?}");
+            server.commit(&user_ic.ic, &commit)?;
+        }
+        eprintln!(
+            "touchdeck-ime: xim key consumed={} preedit={:?}",
+            response.consumed, response.preedit
+        );
+        Ok(response.consumed)
+    }
+}
+
+pub(super) fn spawn_xim_server(tx: Sender<XimRequest>) {
+    thread::spawn(move || {
+        if let Err(err) = run_xim_server(tx) {
+            eprintln!("touchdeck-ime: xim server stopped: {err:?}");
+        }
+    });
+}
+
+fn run_xim_server(tx: Sender<XimRequest>) -> Result<()> {
+    let (conn, screen_num) = x11rb::rust_connection::RustConnection::connect(None)
+        .context("connect to X display for XIM")?;
+    let mut server = xim::x11rb::X11rbServer::init(conn, screen_num, "touchdeck", xim::ALL_LOCALES)
+        .context("initialize XIM server")?;
+    let mut connections = XimConnections::new();
+    let mut handler = TouchDeckXimHandler::new(tx);
+
+    eprintln!("touchdeck-ime: xim server initialized");
+    loop {
+        let event = server.conn().wait_for_event().context("wait for X event")?;
+        match server.filter_event(&event, &mut connections, &mut handler) {
+            Ok(_) => {
+                if let Err(err) = server.conn().flush() {
+                    eprintln!("touchdeck-ime: xim flush error: {err}");
+                }
+            }
+            Err(err) => eprintln!("touchdeck-ime: xim event error: {err}"),
+        }
+    }
+}
